4eb8c65178a762c7bdd11229b9a9dea4c6f1b210
Selçuk İtmiş <slck87@hotmail.com>
2017-05-21 01:26:43 +0300

    Revert "Revert "initial commit""
    This reverts commit 6aca10d678474dc976a763815a6e8202d399051f.

---
+++ MultiLanguage/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiLanguage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChangeLanguage(string language)
+        {
+            HttpCookie cookie;
+            cookie = new HttpCookie("MultiLanguageExample");
+            Thread.CurrentThread.CurrentCulture =new CultureInfo(language);
+            cookie.Value = language;
+            Response.SetCookie(cookie);
+            if (Request.UrlReferrer != null) return Redirect(Request.UrlReferrer.LocalPath);
+            return Redirect("/Home/Index");
+        }
+    }
+}
